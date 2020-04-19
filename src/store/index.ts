58e1a9b3c5ba01c01d3e5a6ff15b762446e9bd46
李光耀 <lgy87@foxmail.com>
2020-04-19 22:50:58 +0800

    [chore]

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,8 @@
-import { routerMiddleware as createRouterMiddleware } from "connected-react-router"
 import { createBrowserHistory } from "history"
-import { RootAction, RootState, Services } from "MyTypes"
 import { applyMiddleware, createStore } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
-import services from "../services"
 import middlewares from "./middlewares"
 import rootReducer from "./rootReducer"
-import { composeEnhancers } from "./utils"
 
 const composeEnhancers = composeWithDevTools({})
 
@@ -15,22 +11,8 @@ export default createStore(
   composeEnhancers(applyMiddleware(...middlewares)),
 )
 
-// browser history
 export const history = createBrowserHistory()
 
-export const epicMiddleware = createEpicMiddleware<
-  RootAction,
-  RootAction,
-  RootState,
-  Services
->({
-  dependencies: services,
-})
-
-const routerMiddleware = createRouterMiddleware(history)
-
-// configure middlewares
-const middlewares = [epicMiddleware, routerMiddleware]
 // compose enhancers
 const enhancer = composeEnhancers(applyMiddleware(...middlewares))
 
@@ -39,8 +21,3 @@ const initialState = {}
 
 // create store
 const store = createStore(rootReducer(history), initialState, enhancer)
-
-epicMiddleware.run(rootEpic)
-
-// export store singleton instance
-export default store
--- src/store/types.ts
+++ src/store/types.ts
@@ -2,13 +2,8 @@ import { ActionType, StateType } from "typesafe-actions"
 
 export type RootAction = ActionType<typeof import("./rootAction").default>
 export type RootReducer = ReturnType<typeof import("./rootReducer").default>
+export type RootState = StateType<RootReducer>
 export type Store = StateType<typeof import("./index").default>
-
-declare module "MyTypes" {
-  export type Store = Store
-  export type RootAction = RootAction
-  export type RootState = StateType<RootReducer>
-}
 
 declare module "typesafe-actions" {
   interface Types {
