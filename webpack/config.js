f05d81d145a52cd66773e9e4267a7639e5c222a7
李光耀 <lgy87@foxmail.com>
2020-04-22 21:55:07 +0800

    [chore] webpack alias

--- webpack/config.js
+++ webpack/config.js
@@ -3,5 +3,8 @@ const { SRC, resolveApp } = require("./paths")
 module.exports.alias = {
   "~": SRC,
   "@main": resolveApp("main"),
+  "@": `${SRC}/apps`,
   "react-dom": "@hot-loader/react-dom",
+  "react-redux":
+    process.env.NODE_ENV === "development" ? "react-redux/lib" : "react-redux",
 }
